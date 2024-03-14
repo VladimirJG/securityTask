17793983be262b42b6b8fd8da3916fde3dff2c8b
VladimirJG <vladimi85@gmail.com>
2024-03-14 07:52:15 +0300

    part 3 added AuthController, form for login page, and modified SecurityConfig (configure(HttpSecurity http))

--- src/main/java/com/example/securityTask/config/SecurityConfig.java
+++ src/main/java/com/example/securityTask/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.example.securityTask.service.PersonDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -19,6 +20,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         this.personDetailsService = personDetailsService;
     }
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // Конфигурция SS (какая стр. отв за вход/ошибки и т.д.)
+        // + в нем идет конфиг авторизации
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/auth/login", "/error").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage("/auth/login")
+                .loginProcessingUrl("/process_login")
+                .defaultSuccessUrl("/hello", true)
+                .failureUrl("/auth/login?error");
+    }
+
+    //Настройка аутентификации
+    @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(personDetailsService);
     }
