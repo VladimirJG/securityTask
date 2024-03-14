6573c36dd15f2dd3ce5ffa4e357383fd88a5278b
VladimirJG <vladimi85@gmail.com>
2024-03-14 09:27:30 +0300

    part 6 Password Encoder mod SecurityConfig(configure(auth), getPasswordEncoder); and RegistrationService(register())

--- src/main/java/com/example/securityTask/service/RegistrationService.java
+++ src/main/java/com/example/securityTask/service/RegistrationService.java
@@ -3,20 +3,25 @@ package com.example.securityTask.service;
 import com.example.securityTask.model.Person;
 import com.example.securityTask.repositry.PeopleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class RegistrationService {
     private final PeopleRepository peopleRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public RegistrationService(PeopleRepository peopleRepository) {
+    public RegistrationService(PeopleRepository peopleRepository, PasswordEncoder passwordEncoder) {
         this.peopleRepository = peopleRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Transactional
     public void register(Person person) {
+        String encodedPassword = passwordEncoder.encode(person.getPassword());
+        person.setPassword(encodedPassword);
         peopleRepository.save(person);
     }
 }
